1a981cdef0c6de627ecbcbadd4b595e7e98c0024
low-sky <rosolowsky@gmail.com>
2014-06-09 20:35:43 -0600

    Added bary2lsr.py

--- jcmt/bary2lsr.py
+++ jcmt/bary2lsr.py
@@ -5,8 +5,37 @@ from astropy.coordinates import FK5,SkyCoord
 import astropy.units as u
 from math import cos
 
-
 def bary2lsr(hdr):
+    """
+    Given a FITS header, this function returns the magnitude of the
+    correction required to convert velocities in the BARYCENT velocity
+    frame into velocities in the LSRK frame.
+
+    This velocity shift should be ADDED to velocities to move from
+    BARYCENT -> LSRK and SUBTRACTED to convert LSRK -> BARYCENT
+
+    Definitions taken from:
+    http://www.gb.nrao.edu/~fghigo/gbtdoc/doppler.html
+
+    Usage:
+
+    unix> bary2lsr.py fits_file_name.fits
+    To convert BARYCENT to LSRK, add 18.0701766541 km / s to the
+    velocity.
+
+    python% bary2lsr(hdr)
+    18.0701766541 km/ s
+
+    Parameters
+    ----------
+    hdr : FITS header string
+
+    Returns
+    -------
+    offset : astropy.units quantity of the magnitude of the velocity
+    shift.  
+    
+    """
     Obs_Direction = SkyCoord(hdr['CRVAL1'],hdr['CRVAL2'],frame='fk5',
                              unit="deg")
     LSR_Direction = SkyCoord("18h03m50.29s +30d00m16.8s",frame='fk5')
