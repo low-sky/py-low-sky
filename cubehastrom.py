c708e8f49d302937ad4a400a786d52d5f825e6fc
Erik Rosolowsky <rosolowsky@ualberta.ca>
2017-03-18 01:10:26 -0600

    Cube HASTROM

--- cubehastrom.py
+++ cubehastrom.py
@@ -3,7 +3,7 @@ from skimage import transform as tf
 import astropy.wcs as wcs
 import numpy as np
 
-def pyhastrom(WarpHDU, TemplateHDU, anchors = 5, order = 2):
+def cubehastrom(WarpHDU, TemplateHDU, anchors = 5, order = 2):
 
     normal = np.linspace(0,1,anchors)
     naxis = WarpHDU.header.get('NAXIS', 0)
@@ -29,7 +29,8 @@ def pyhastrom(WarpHDU, TemplateHDU, anchors = 5, order = 2):
     # Build Output Image
     OutputSize = ((np.c_[size,sizeTemplate]).max(axis=1))
     # Switch first two elements to make array sizes pythonic
-    OutputSize[0],OutputSize[1] = OutputSize[1],OutputSize[0]
+    OutputSize[0],OutputSize[1],OutputSize[2] = OutputSize[2],OutputSize[1],OutputSize[0]
+    
     OutputImg = np.zeros(OutputSize)
 
     # Calculate XY positions for anchor points in image
@@ -43,14 +44,16 @@ def pyhastrom(WarpHDU, TemplateHDU, anchors = 5, order = 2):
     TemplateWCS = wcs.WCS(TemplateHDU.header)
     WarpWorld = WarpWCS.wcs_pix2world(*XY)
     WarpWorld += (0,)
+    import pdb; pdb.set_trace()
     TemplateXY = TemplateWCS.wcs_world2pix(*WarpWorld)
     # Assemble mapping coordinates
-    src = np.c_[XY[0].flatten(),XY[1].flatten()]
-    dst = np.c_[TemplateXY[0].flatten(),TemplateXY[1].flatten()]
+    src = np.c_[XY[0].flatten(),XY[1].flatten(),XY[2].flatten()]
+    dst = np.c_[TemplateXY[0].flatten(),TemplateXY[1].flatten(),TemplateXY[2].flatten()]
 
     # Exectue Transform
     xform = tf.estimate_transform('polynomial', dst, src, order = order)
-    OutputImg[0:size[1],0:size[0]] = WarpHDU.data
+    import pdb; pdb.set_trace()
+    OutputImg[0:size[2],0:size[1],0:size[0]] = WarpHDU.filled_data[:]
     OutputImg = tf.warp(OutputImg, inverse_map=xform) 
 
     # Crop image to template size
